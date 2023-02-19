f9df879885f455a2ca388d421859746abf066a58
faiyaz_inspiron <faiyaz_r@outlook.com>
2023-02-19 12:20:30 +0800

    feat: added google auth

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import { signIn } from 'next-auth/react'
+import React from 'react'
+
+import { BsTwitter } from 'react-icons/bs'
+
+const Sidebar = () => {
+  return (
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl-w-[340px] p-2 fixed h-full border-r border-gray-400 pr-0 xl:pr-8'>
+      <div className='flex items-center justify-center w-14 h-14 hoverEffect p-0 xl:ml-24'>
+        <BsTwitter className='text-white-[34px]'/>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
