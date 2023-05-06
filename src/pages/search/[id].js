15fc5a6bee463c81a6ab43b2e53beb86ce67a7b0
viscory <47732532+viscory@users.noreply.github.com>
2023-05-06 13:25:07 +0800

    Update [id].js

--- src/pages/search/[id].js
+++ src/pages/search/[id].js
@@ -6,7 +6,7 @@ import { onSnapshot, collection, query, where, doc, updateDoc, orderBy } from 'f
 import { db } from '@/firebase' 
 import { useRouter } from 'next/router'
 
-//our implementation to be able to search for users
+// Component for the user search page
 export default function UserSearch () {
   const [result, setResult] = useState([])
   const [following, setFollowing] = useState([])
@@ -15,7 +15,7 @@ export default function UserSearch () {
   const router = useRouter()
   const searchKeyWord = window.location.pathname.split('/')[2]
 
-  //we actually lcase the search query then match it, we used regex before but it felt too unreadable
+  // Search for users whose tags contain the search keyword
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setUserId(localStorage.getItem('userId'))
@@ -34,6 +34,7 @@ export default function UserSearch () {
     }
   }, [userId])
 
+  // Get the following list of the current user and set the user object ID
   useEffect(() => {
     if (userId) {
       setUserId(localStorage.getItem('userId'))
@@ -50,7 +51,7 @@ export default function UserSearch () {
     }
   }, [result])
 
-  //after searching for users we give them the option to interact with the user too
+  // Follow a user by adding their ID to the following list
   const followUser = async (account) => {
     console.log(userObjectId)
     await updateDoc(doc(db, 'users', userObjectId), {
@@ -58,19 +59,25 @@ export default function UserSearch () {
     })
   }
 
+  // Unfollow a user by removing their ID from the following list
   const unfollowUser = async (account) => {
     await updateDoc(doc(db, 'users', userObjectId), {
       following: following.filter((id) => id !== account.userId)
     })
   }
 
+  // Set mount flag to true after component has mounted
   const [hasMounted, setHasMounted] = React.useState(false)
   React.useEffect(() => {
     setHasMounted(true)
   }, [])
+
+  // If component has not yet mounted, return null
   if (!hasMounted) {
     return null
   }
+
+  // Return the main content of the user search page
   return (
     <div className="bg-white dark:bg-black">
       <Head>
@@ -82,68 +89,69 @@ export default function UserSearch () {
         <Sidebar/>
         <div className='flex gap-6 text-black dark:text-white'>
           <div className='sm:ml-20 xl:ml-[340px] w-[600px] min-h-screen border-r border-gray-800 dark:border-gray-400 p-2'>
-          {
+            {/* If there are search results, display them */}
+            {
               result.length > 0
                 ? (
                     result.map((account) => {
                       return (
-                    <div
-                      className="border-b-2 border-gray-800 dark:border-gray-400 py-3 flex cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-900"
-                      onClick={() => router.push(`/profile/${account.tag}`)}
-                    >
-                      <img
-                        src={account.userImg}
-                        className="rounded-full w-7 h-7 mr-4 my-auto"
-                      />
-                      <div className="text-lg mr-4 my-auto">{account.username}</div>
-                      <div className="text-lg my-auto">@{account.tag}</div>
-                      {
-                          account.userId === userId
+                        <div
+                          className="border-b-2 border-gray-800 dark:border-gray-400 py-3 flex cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-900"
+                          onClick={() => router.push(`/profile/${account.tag}`)}
+                        >
+                          <img
+                            src={account.userImg}
+                            className="rounded-full w-7 h-7 mr-4 my-auto"
+                          />
+                          <div className="text-lg mr-4 my-auto">{account.username}</div>
+                          <div className="text-lg my-auto">@{account.tag}</div>
+                          {
+                            account.userId === userId
                             ? (null)
                             : following.includes(account.userId)
                               ? (
-                            <>
-
-                            <div className="action-button"
-                              onClick={(e) => {
-                                e.stopPropagation()
-                                router.push(`/chat/${userId}/${account.userId}`)
-                              }}
-                            >
-                              Chat
-                            </div>
-                            <div className="action-button" onClick={(e) => {
-                              e.stopPropagation()
-                              unfollowUser(account)
-                            }}>Unfollow</div>
-                            </>
-                                )
+                                <>
+                                  <div className="action-button"
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      router.push(`/chat/${userId}/${account.userId}`)
+                                    }}
+                                  >
+                                    Chat
+                                  </div>
+                                  <div className="action-button" onClick={(e) => {
+                                    e.stopPropagation()
+                                    unfollowUser(account)
+                                  }}>Unfollow</div>
+                                </>
+                              )
                               : (
-                            <>
-                            <div className="action-button"
-                              onClick={(e) => {
-                                e.stopPropagation()
-                                router.push(`/chat/${userId}/${account.userId}`)
-                              }}
-                            >
-                              Chat
-                            </div>
-                              <div className="action-button" onClick={(e) => {
-                                e.stopPropagation()
-                                followUser(account)
-                              }}>Follow</div>
-                            </>
-                                )
-                            }
-                    </div>
+                                <>
+                                  <div className="action-button"
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      router.push(`/chat/${userId}/${account.userId}`)
+                                    }}
+                                  >
+                                    Chat
+                                  </div>
+                                  <div className="action-button" onClick={(e) => {
+                                    e.stopPropagation()
+                                    followUser(account)
+                                  }}>Follow</div>
+                                </>
+                              )
+                          }
+                        </div>
                       )
                     })
                   )
+                // If there are no search results, display a message
                 : (
-                <div className="text-lg text-center py-4">
-                  No accounts found.
-                </div>
-                  )
+                  <div className="text-lg text-center py-4">
+                    No accounts found.
+                  </div>
+                )
             }
           </div>
           <Trending />
