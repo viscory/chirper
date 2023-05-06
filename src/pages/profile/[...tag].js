795e19db3ac9d9675e82824203367eaa21397f43
viscory <47732532+viscory@users.noreply.github.com>
2023-05-06 13:26:41 +0800

    Update [...tag].js

--- src/pages/profile/[...tag].js
+++ src/pages/profile/[...tag].js
@@ -6,19 +6,23 @@ import Profile from '@/components/Chirps/Profile'
 import { AppContext } from '@/contexts/AppContext'
 import Trending from '@/components/Common/Trending'
 
-//boilerplate
 const ProfilePage = () => {
 
+  // Get the current app context
   const [appContext] = useContext(AppContext)
 
+  // Set mount flag to true after component has mounted
   const [hasMounted, setHasMounted] = React.useState(false);
   React.useEffect(() => {
     setHasMounted(true);
   }, []);
+
+  // If component has not yet mounted, return null
   if (!hasMounted) {
     return null;
   }
 
+  // Return the main content of the profile page
   return (
     <div>
       <Head>
@@ -27,14 +31,14 @@ const ProfilePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='relative max-w-[1400px] mx-auto'>
-          <Sidebar/>
-          <div className='flex gap-6'>
-            <div className='sm:ml-20 xl:ml-[340px] w-[600px] min-h-screen border-r border-gray-400 text-white py-2'>
-              <Profile />
-            </div>
-            <Trending />
+        <Sidebar/>
+        <div className='flex gap-6'>
+          <div className='sm:ml-20 xl:ml-[340px] w-[600px] min-h-screen border-r border-gray-400 text-white py-2'>
+            <Profile />
           </div>
-        </main>
+          <Trending />
+        </div>
+      </main>
     </div>
   )
 }
