4d24e88ba318fdec9b12014a58fdfe42b8074a58
viscory <47732532+viscory@users.noreply.github.com>
2023-05-06 13:12:35 +0800

    Update SidebarLink.js

--- src/components/Common/SidebarLink.js
+++ src/components/Common/SidebarLink.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-//the default styling for each link in the sidebar
+// Define SidebarLink component
 const SidebarLink = ({ text, Icon }) => {
+  // Render the SidebarLink component
   return (
     <div className='text-black dark:text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 cursor-pointer px-4 py-2'>
-      <Icon />
-      <span className='hidden xl:inline'>{text}</span>
+      <Icon /> {/* Render the icon passed to the component */}
+      <span className='hidden xl:inline'>{text}</span> {/* Render the text passed to the component */}
     </div>
-  )
-}
+  );
+};
 
-export default SidebarLink
+export default SidebarLink; // Export the SidebarLink component
